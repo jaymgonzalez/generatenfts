e61c83770f62ea4dc1d7e5142469ccd98a7a6680
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-05 12:32:28 +0100

    add deploy script + name change

--- hardhat/contracts/AINFTs.sol
+++ hardhat/contracts/AINFTs.sol
@@ -5,11 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-error AINFT__MaxTokenIdsReached();
-error AINFT__NotEnoughFunds();
-error AINFT__TransactionNotSent();
+error AINFTs__MaxTokenIdsReached();
+error AINFTs__NotEnoughFunds();
+error AINFTs__TransactionNotSent();
 
-contract AINFT is ERC721URIStorage, Ownable {
+contract AINFTs is ERC721URIStorage, Ownable {
     uint256 public _tokenIds;
     uint256 public maxTokenIds = 100;
     uint256 public _price = 0.01 ether;
@@ -23,10 +23,10 @@ contract AINFT is ERC721URIStorage, Ownable {
         returns (uint256)
     {
         if (_tokenIds > maxTokenIds) {
-            revert AINFT__MaxTokenIdsReached();
+            revert AINFTs__MaxTokenIdsReached();
         }
         if (msg.value < _price) {
-            revert AINFT__NotEnoughFunds();
+            revert AINFTs__NotEnoughFunds();
         }
         _tokenIds += 1;
 
@@ -41,7 +41,7 @@ contract AINFT is ERC721URIStorage, Ownable {
         uint256 amount = address(this).balance;
         (bool sent, ) = _owner.call{value: amount}("");
         if (!sent) {
-            revert AINFT__TransactionNotSent();
+            revert AINFTs__TransactionNotSent();
         }
     }
 
