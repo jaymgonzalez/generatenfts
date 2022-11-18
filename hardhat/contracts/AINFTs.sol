476aef21c18d7b92a514b3249f8a9b8d16556ff5
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-18 15:45:18 +0100

    add erc20 token to constructor

--- hardhat/contracts/AINFTs.sol
+++ hardhat/contracts/AINFTs.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 error AINFTs__MaxTokenIdsReached();
 error AINFTs__NotEnoughFunds();
@@ -13,8 +14,11 @@ contract AINFTs is ERC721URIStorage, Ownable {
     uint256 public _tokenIds;
     uint256 public maxTokenIds = 100;
     uint256 public _price = 0.01 ether;
+    IERC20 public tokenAddress;
 
-    constructor() ERC721("AINFTs", "AINFT") {}
+    constructor(address _tokenAddress) ERC721("AINFTs", "AINFT") {
+        tokenAddress = IERC20(_tokenAddress);
+    }
 
     function mintNFT(address recipient, string memory tokenURI)
         public
