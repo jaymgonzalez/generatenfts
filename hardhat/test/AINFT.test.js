13f1d252e899d3e14e8ea0cdde680f906ff607a3
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-19 18:39:13 +0100

    2 passing tests :)

--- hardhat/test/AINFT.test.js
+++ hardhat/test/AINFT.test.js
@@ -19,16 +19,27 @@ const POLYGON_MUMBAI_LINK_ADDRESS =
         addr3,
         addrs
       beforeEach(async function () {
-        AINFT = await ethers.getContractFactory('AINFTs')
-        Link = await ethers.getContractFactory('Link')
         ;[owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners()
+        AINFT = await ethers.getContractFactory('AINFTs')
+        Link = await ethers.getContractFactory('Link', owner)
         linkContract = await Link.deploy()
+        await linkContract.deployed()
+
+        console.log(await linkContract.balanceOf(owner.address))
+
         nftContract = await AINFT.deploy(linkContract.address)
       })
 
       describe('Deployment', function () {
         it('Should set the right owner', async function () {
           expect(await nftContract.owner()).to.equal(owner.address)
+          // console.log(owner)
+          // console.log(await owner.address.getBalance(linkContract.address))
+        })
+        it('Should have minted Link tokens', async function () {
+          expect(await linkContract.balanceOf(owner.address)).to.equal(
+            '1000000000000000000000000'
+          )
         })
       })
     })
