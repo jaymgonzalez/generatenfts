0d98ebc3df9b2bfe1b1ad5132da7ed1b66341ca6
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-19 10:37:08 +0100

    playing with IERC20 - adding it to the contract

--- hardhat/contracts/AINFTs.sol
+++ hardhat/contracts/AINFTs.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 error AINFTs__MaxTokenIdsReached();
 error AINFTs__NotEnoughFunds();
@@ -13,28 +14,38 @@ error AINFTs__TransactionNotSent();
 contract AINFTs is ERC721URIStorage, Ownable {
     uint256 public _tokenIds;
     uint256 public maxTokenIds = 100;
-    uint256 public _price = 0.1 ether;
-    IERC20 public tokenAddress;
+    uint256 public _price = 0.01 ether;
+    address public tokenAddress;
 
     constructor(address _tokenAddress) ERC721("AINFTs", "AINFT") {
-        tokenAddress = IERC20(_tokenAddress);
+        tokenAddress = _tokenAddress;
     }
 
     function mintNFT(address recipient, string memory tokenURI)
         public
         payable
-        onlyOwner
         returns (uint256)
     {
+        address sender = msg.sender;
+
         if (_tokenIds > maxTokenIds) {
             revert AINFTs__MaxTokenIdsReached();
         }
-        if (msg.value < _price) {
-            revert AINFTs__NotEnoughFunds();
-        }
+        // if (msg.value < _price) {
+        //     revert AINFTs__NotEnoughFunds();
+        // }
+
+        console.log("Token address is: %s", tokenAddress);
+
+        console.log(
+            "Token address balance is: %s",
+            IERC20(tokenAddress).balanceOf(sender)
+        );
+
+        require(_price >= IERC20(tokenAddress).balanceOf(sender));
         _tokenIds += 1;
 
-        tokenAddress.transferFrom(msg.sender, address(this), _price);
+        IERC20(tokenAddress).transferFrom(sender, address(this), _price);
         _mint(recipient, _tokenIds);
         _setTokenURI(_tokenIds, tokenURI);
 
--- hardhat/scripts/mint-nft.js
+++ hardhat/scripts/mint-nft.js
@@ -1,28 +1,29 @@
-const { getNamedAccounts, ethers } = require('hardhat')
+const { getNamedAccounts, ethers, provider } = require('hardhat')
 const { utils } = require('ethers')
 const contract = require('../artifacts/contracts/AINFTs.sol/AINFTs.json')
-const contractAddress = '0x4a2fc7340C5C8A0C2b70c02f1287ACAB9Eedfc29'
+const contractAddress = '0xc985d5f2f1144acBbc7781bb0D085d6a1Aa23baE'
 require('dotenv').config({ path: '.env' })
 
 const ETH_ADDRESS = process.env.ETH_ADDRESS
 const metadataURL = 'ipfs://QmaEH4QgkSWeQaM9aDEhhDTcz5wEiHfdqaYpHJyGgFsev4'
 
 async function main() {
-  const { deployer } = await getNamedAccounts()
+  const signer = await ethers.getSigner()
 
   const AINFT = await ethers.getContractAt(
     contract.abi,
     contractAddress,
-    deployer
+    signer
   )
   console.log('Minting NFT...')
 
-  const tx = await AINFT.mintNFT(ETH_ADDRESS, metadataURL, {
+  const tx = await AINFT.mintNFT(signer.address, metadataURL, {
     value: utils.parseEther('0.01'),
+    gasLimit: 5000000,
   })
 
   console.log(
-    `Check yout transaction at https://goerli.etherscan.io/tx/${tx.hash}`
+    `Check yout transaction at https://mumbai.polygonscan.com/tx/${tx.hash}`
   )
 
   await tx.wait()
