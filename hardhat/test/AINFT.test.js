abfffff92a5240716ef20ad5be0368d5d6d9e3e3
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-19 17:33:18 +0100

    rename file

--- hardhat/test/AINFT.test.js
+++ hardhat/test/AINFT.test.js
@@ -1,9 +1,6 @@
 const { assert, expect } = require('chai')
 const { network, deployments, ethers } = require('hardhat')
-const {
-  developmentChains,
-  networkConfig,
-} = require('../../helper-hardhat-config')
+const { developmentChains, networkConfig } = require('../helper-hardhat-config')
 require('dotenv').config()
 
 const POLYGON_MUMBAI_LINK_ADDRESS =
@@ -12,5 +9,16 @@ const POLYGON_MUMBAI_LINK_ADDRESS =
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe('AINFTs Unit Tests', function () {
-      let
+      let AINFT, contract
+      beforeEach(async function () {
+        AINFT = await ethers.getContractFactory('AINFTs')
+        ;[owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners()
+        contract = await AINFT.deploy()
+      })
+
+      describe('Deployment', function () {
+        it('Should set the right owner', async function () {
+          expect(await contract.owner.to.equal(owner.address))
+        })
+      })
     })
