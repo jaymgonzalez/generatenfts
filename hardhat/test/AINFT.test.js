7779d2cff26d2fd68da8770de06b26f082d3a623
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-26 23:25:27 +0100

    add tests

--- hardhat/test/AINFT.test.js
+++ hardhat/test/AINFT.test.js
@@ -20,10 +20,6 @@ require('dotenv').config()
         AINFTs = await ethers.getContractFactory('AINFTs')
         Link = await ethers.getContractFactory('Link', owner)
         linkContract = await Link.deploy()
-        await linkContract.deployed()
-
-        // console.log(await linkContract.balanceOf(owner.address))
-
         nftContract = await AINFTs.deploy()
       })
 
@@ -36,13 +32,26 @@ require('dotenv').config()
             '1000000000000000000000000'
           )
         })
-        it('Should add a token to the token list', async function () {
+        it('Should add a token to the token list if owner', async function () {
           await nftContract.addCurrency(
             linkContract.address,
             ethers.utils.parseEther('1'),
             'Link 3'
           )
           const approvedTokens = await nftContract.getApprovedTokenInfo()
+
+          expect(approvedTokens[0][0]).to.equal(linkContract.address)
+          expect(approvedTokens[1][0]).to.equal('1000000000000000000')
+          expect(approvedTokens[2][0]).to.equal('Link 3')
+        })
+        it.only('Should NOT add a token to the token list if NOT owner', async function () {
+          await nftContract
+            .connect(addr1)
+            .addCurrency(
+              linkContract.address,
+              ethers.utils.parseEther('1'),
+              'Link 3'
+            )
 
           expect(approvedTokens[0][0]).to.equal(linkContract.address)
           expect(approvedTokens[1][0]).to.equal('1000000000000000000')
@@ -64,20 +73,25 @@ require('dotenv').config()
 
           expect(await nftContract.balanceOf(addr1.address)).to.equal(1)
         })
-        // it('Should allow transfer of Link tokens', async function () {
-        //   await linkContract.approve(
-        //     owner.address,
-        //     ethers.utils.parseEther('100')
-        //   )
-        //   await linkContract.transferFrom(
-        //     owner.address,
-        //     addr2.address,
-        //     ethers.utils.parseEther('100')
-        //   )
-        //   const balance = await linkContract.balanceOf(addr2.address)
-        //   console.log(balance)
-        //   expect(await linkContract.balanceOf(addr2.address).to.equal(balance))
-        // })
+        it.only('Should allow withdraws from owner', async function () {
+          await linkContract.approve(
+            owner.address,
+            ethers.utils.parseEther('100')
+          )
+          await linkContract.transferFrom(
+            owner.address,
+            addr2.address,
+            ethers.utils.parseEther('100')
+          )
+
+          expect(await linkContract.balanceOf(addr2.address)).to.equal(
+            '100000000000000000000'
+          )
+
+          // const balance = await linkContract.balanceOf(addr2.address)
+          // console.log(balance)
+          // expect(await linkContract.balanceOf(addr2.address).to.equal(balance))
+        })
         // it('Should allow transfer of Link tokens', async function () {
         //   const baseurl = 'ipfs://test.url/'
         //   const overrides = {
