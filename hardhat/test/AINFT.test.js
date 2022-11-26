87831055c72580a614032108551234acb3a91d91
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-26 23:11:43 +0100

    add tests

--- hardhat/test/AINFT.test.js
+++ hardhat/test/AINFT.test.js
@@ -3,13 +3,10 @@ const { network, deployments, ethers } = require('hardhat')
 const { developmentChains, networkConfig } = require('../helper-hardhat-config')
 require('dotenv').config()
 
-const POLYGON_MUMBAI_LINK_ADDRESS =
-  process.env.POLYGON_MUMBAI_LINK_ADDRESS || ''
-
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe('AINFTs Unit Tests', function () {
-      let AINFT,
+      let AINFTs,
         Link,
         nftContract,
         linkContract,
@@ -20,14 +17,14 @@ const POLYGON_MUMBAI_LINK_ADDRESS =
         addrs
       beforeEach(async function () {
         ;[owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners()
-        AINFT = await ethers.getContractFactory('AINFTs')
+        AINFTs = await ethers.getContractFactory('AINFTs')
         Link = await ethers.getContractFactory('Link', owner)
         linkContract = await Link.deploy()
         await linkContract.deployed()
 
-        console.log(await linkContract.balanceOf(owner.address))
+        // console.log(await linkContract.balanceOf(owner.address))
 
-        nftContract = await AINFT.deploy(linkContract.address)
+        nftContract = await AINFTs.deploy()
       })
 
       describe('Deployment', function () {
@@ -39,53 +36,66 @@ const POLYGON_MUMBAI_LINK_ADDRESS =
             '1000000000000000000000000'
           )
         })
-        it('Should fail if not enough Link for transaction', async function () {
-          const baseurl = 'ipfs://test.url/'
-          const overrides = {
-            value: ethers.utils.parseEther('0.01'),
-          }
-          expect(
-            await nftContract
-              .connect(owner)
-              .mintNFT(addr1.address, baseurl, overrides)
-          ).to.be.revertedWith('ERC20: insufficient allowance')
-        })
-        it.only('Should allow transfer of Link tokens', async function () {
-          await linkContract.approve(
-            owner.address,
-            ethers.utils.parseEther('100')
+        it('Should add a token to the token list', async function () {
+          await nftContract.addCurrency(
+            linkContract.address,
+            ethers.utils.parseEther('1'),
+            'Link 3'
           )
-          await linkContract.transferFrom(
-            owner.address,
-            addr2.address,
-            ethers.utils.parseEther('100')
-          )
-          const balance = await linkContract.balanceOf(addr2.address)
-          console.log(balance)
-          expect(
-            await linkContract
-              .balanceOf(addr2.address)
-              .to.equal('100000000000000000000')
-          )
-        })
-        it('Should allow transfer of Link tokens', async function () {
-          const baseurl = 'ipfs://test.url/'
-          const overrides = {
-            value: ethers.utils.parseEther('0.01'),
-          }
-          await linkContract
-            .connect(addr1)
-            .approve(owner.address, ethers.utils.parseEther('100'))
-          await linkContract.transferFrom(
-            addr1.address,
-            addr2.address,
-            ethers.utils.parseEther('100')
-          )
-          await nftContract
-            .connect(owner)
-            .mintNFT(owner.address, baseurl, overrides)
+          const approvedTokens = await nftContract.getApprovedTokenInfo()
 
-          expect(await nftContract.ownerOf(0)).to.equal(owner.address)
+          expect(approvedTokens[0][0]).to.equal(linkContract.address)
+          expect(approvedTokens[1][0]).to.equal('1000000000000000000')
+          expect(approvedTokens[2][0]).to.equal('Link 3')
         })
+        it('Should mint the NFT', async function () {
+          await nftContract.addCurrency(
+            linkContract.address,
+            ethers.utils.parseEther('1'),
+            'Link 3'
+          )
+
+          await linkContract.approve(
+            nftContract.address,
+            ethers.utils.parseEther('100')
+          )
+
+          await nftContract.mint(addr1.address, 1, 0)
+
+          expect(await nftContract.balanceOf(addr1.address)).to.equal(1)
+        })
+        // it('Should allow transfer of Link tokens', async function () {
+        //   await linkContract.approve(
+        //     owner.address,
+        //     ethers.utils.parseEther('100')
+        //   )
+        //   await linkContract.transferFrom(
+        //     owner.address,
+        //     addr2.address,
+        //     ethers.utils.parseEther('100')
+        //   )
+        //   const balance = await linkContract.balanceOf(addr2.address)
+        //   console.log(balance)
+        //   expect(await linkContract.balanceOf(addr2.address).to.equal(balance))
+        // })
+        // it('Should allow transfer of Link tokens', async function () {
+        //   const baseurl = 'ipfs://test.url/'
+        //   const overrides = {
+        //     value: ethers.utils.parseEther('0.01'),
+        //   }
+        //   await linkContract
+        //     .connect(addr1)
+        //     .approve(owner.address, ethers.utils.parseEther('100'))
+        //   await linkContract.transferFrom(
+        //     addr1.address,
+        //     addr2.address,
+        //     ethers.utils.parseEther('100')
+        //   )
+        //   await nftContract
+        //     .connect(owner)
+        //     .mintNFT(owner.address, baseurl, overrides)
+
+        //   expect(await nftContract.ownerOf(0)).to.equal(owner.address)
+        // })
       })
     })
