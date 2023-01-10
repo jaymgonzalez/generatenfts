408f3bdbb1590451ad779834f8be810bb10857be
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-10 19:25:54 +0100

    add confirmation before refresh page

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -5,6 +5,7 @@ import { MantineProvider } from '@mantine/core'
 import { WagmiConfig } from 'wagmi'
 import { client, chains } from '../wagmi'
 import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit'
+import { NotificationsProvider } from '@mantine/notifications'
 // import { RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth'
 // import { SessionProvider } from 'next-auth/react'
 // import { Session } from 'next-auth'
@@ -24,6 +25,17 @@ export default function App(props) {
 
   useEffect(() => {
     setShowChild(true)
+  }, [])
+
+  useEffect(() => {
+    const unloadCallback = (event) => {
+      event.preventDefault()
+      event.returnValue = ''
+      return ''
+    }
+
+    window.addEventListener('beforeunload', unloadCallback)
+    return () => window.removeEventListener('beforeunload', unloadCallback)
   }, [])
 
   if (!showChild) {
@@ -52,22 +64,24 @@ export default function App(props) {
               // },
             }}
           >
-            <WagmiConfig client={client}>
-              {/* <SessionProvider session={pageProps.session} refetchInterval={0}>  <RainbowKitSiweNextAuthProvider> */}
-              <RainbowKitProvider
-                theme={darkTheme({
-                  accentColor: '#1971C2',
-                  accentColorForeground: 'white',
-                  fontStack: 'system',
-                })}
-                modalSize="compact"
-                chains={chains}
-              >
-                <Navbar />
-                <Component {...pageProps} />
-              </RainbowKitProvider>
-              {/* </RainbowKitSiweNextAuthProvider> </SessionProvider> */}
-            </WagmiConfig>
+            <NotificationsProvider>
+              <WagmiConfig client={client}>
+                {/* <SessionProvider session={pageProps.session} refetchInterval={0}>  <RainbowKitSiweNextAuthProvider> */}
+                <RainbowKitProvider
+                  theme={darkTheme({
+                    accentColor: '#1971C2',
+                    accentColorForeground: 'white',
+                    fontStack: 'system',
+                  })}
+                  modalSize="compact"
+                  chains={chains}
+                >
+                  <Navbar />
+                  <Component {...pageProps} />
+                </RainbowKitProvider>
+                {/* </RainbowKitSiweNextAuthProvider> </SessionProvider> */}
+              </WagmiConfig>
+            </NotificationsProvider>
           </MantineProvider>
         </Provider>
       </>
