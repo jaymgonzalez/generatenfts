de6beb5586b73b243af2e3d4cdcaa95e37380121
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-10 05:31:24 +0100

    add cloudflare worker

---
+++ worker/uploading_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the name of the directory to be uploaded
+UPLOAD_DIR=$(pwd)'/out'
+
+# Iterate through all files in the directory
+for file in $(find "$UPLOAD_DIR" -type f); do
+  # Get the file name
+  filename=$(basename "$file")
+
+  # Get the subdirectory path and remove the leading "./"
+  subdir_path=$(echo "$file" | sed 's|/home/jay-pc/workspace/earnWeb3/ai-nft/front-end/out||')
+
+  # Construct the full file name with the subdirectory path
+  full_filename=${subdir_path#'/'}
+
+  # Upload the file to Cloudflare Workers
+  curl -X PUT "https://generatenfts.jaimedevelops.workers.dev/$full_filename" --data-binary "@$file"
+done
