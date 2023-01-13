cd568a8a41b18816ea8f28a27ac4032ed9a61e96
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-13 02:42:55 +0100

    fix worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,15 +1,6 @@
 const destinationURL = 'https://generatenfts.xyz/index.html'
 const statusCode = 301
 
-async function handleRequest(request) {
-  return Response.redirect(destinationURL, statusCode)
-}
-
-addEventListener('fetch', async (event) => {
-  // @ts-ignore
-  event.respondWith(handleRequest(event.request))
-})
-
 const hasValidHeader = (request, env) => {
   return request.headers.get('X-Custom-Auth-Key') === env.AUTH_KEY_SECRET
 }
@@ -24,46 +15,50 @@ function authorizeRequest(request, env, key) {
   }
 }
 
-// export default {
-//   async fetch(request, env) {
-//     const url = new URL(request.url)
-//     const key = url.pathname.slice(1)
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url)
+    const key = url.pathname.slice(1)
 
-//     if (!authorizeRequest(request, env, key)) {
-//       return new Response('Forbidden', { status: 403 })
-//     }
+    if (request.method === 'GET') {
+      return Response.redirect(destinationURL, statusCode)
+    }
 
-//     switch (request.method) {
-//       case 'PUT':
-//         await env.MY_BUCKET.put(key, request.body)
-//         return new Response(`Put ${key} successfully!\n`)
-//       case 'GET':
-//         const object = await env.MY_BUCKET.get(key)
+    if (!authorizeRequest(request, env, key)) {
+      return new Response('Forbidden', { status: 403 })
+    }
 
-//         if (object === null) {
-//           return new Response('Object Not Found', { status: 404 })
-//         }
+    switch (request.method) {
+      case 'PUT':
+        await env.MY_BUCKET.put(key, request.body)
+        return new Response(`Put ${key} successfully!\n`)
+      case 'GET':
+        const object = await env.MY_BUCKET.get(key)
 
-//         const headers = new Headers()
-//         object.writeHttpMetadata(headers)
-//         headers.set('etag', object.httpEtag)
+        if (object === null) {
+          return new Response('Object Not Found', { status: 404 })
+        }
 
-//         return new Response(object.body, {
-//           headers,
-//         })
-//       case 'DELETE':
-//         await env.MY_BUCKET.delete(key)
-//         return new Response('Deleted!')
+        const headers = new Headers()
+        object.writeHttpMetadata(headers)
+        headers.set('etag', object.httpEtag)
 
-//       default:
-//         return new Response('Method Not Allowed', {
-//           status: 405,
-//           headers: {
-//             Allow: 'PUT, GET, DELETE',
-//           },
-//         })
-//     }
-//   },
-// }
+        return new Response(object.body, {
+          headers,
+        })
+      case 'DELETE':
+        await env.MY_BUCKET.delete(key)
+        return new Response('Deleted!')
 
-export {}
+      default:
+        return new Response('Method Not Allowed', {
+          status: 405,
+          headers: {
+            Allow: 'PUT, GET, DELETE',
+          },
+        })
+    }
+  },
+}
+
+// export {}
