4b03d6d3800cb2ae750910e77a66d977842fe58b
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-25 11:30:35 +0100

    add nfts store configs

---
+++ front-end/store/slices/nftSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { HYDRATE } from 'next-redux-wrapper'
+
+const initialState = {
+  ownedNfts: [],
+}
+
+export const nftSlice = createSlice({
+  name: 'nfts',
+  initialState,
+  reducers: {
+    setNftData: (state, action) => {
+      state.ownedNfts = action.payload
+    },
+
+    // Special reducer for hydrating the state
+    // extraReducers: {
+    //   // @ts-ignore
+    //   [HYDRATE]: (state, action) => {
+    //     return {
+    //       ...state,
+    //       ...action.payload.images,
+    //     }
+    //   },
+    // },
+  },
+})
+
+export const { setNftData } = nftSlice.actions
+export const selectNftData = (state) => state.images.ownedNfts
+export default nftSlice.reducer
--- front-end/store/store.ts
+++ front-end/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { createWrapper } from 'next-redux-wrapper'
 import { imageSlice } from './slices/imageSlice'
+import { nftSlice } from './slices/nftSlice'
 
 const makeStore = () =>
   configureStore({
     reducer: {
       [imageSlice.name]: imageSlice.reducer,
+      [nftSlice.name]: nftSlice.reducer,
     },
     devTools: true,
   })
