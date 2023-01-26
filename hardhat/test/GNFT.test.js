148e23144b75e2d035cd42cfdd978e899483c889
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-26 10:23:14 +0100

    add withdraw tests

--- hardhat/test/GNFT.test.js
+++ hardhat/test/GNFT.test.js
@@ -70,7 +70,7 @@ describe('GNFT Unit Tests', function () {
     })
   })
 
-  describe.only('Only Owner', function () {
+  describe('Only Owner', function () {
     it('should pause contract execution', async () => {
       await gnft.pause()
       expect(await gnft.paused()).to.equal(true)
@@ -93,104 +93,32 @@ describe('GNFT Unit Tests', function () {
     })
   })
 
-  // console.log(receipt)
-  // expect(receipt.logs).to.have.lengthOf(1)
-  // const event = receipt.logs[0]
-  // expect(event.event).to.be.equal('Transfer')
-  // expect(event.args.from).to.be.equal(
-  //   '0x0000000000000000000000000000000000000000'
-  // )
-  // expect(event.args.to).to.be.equal(other)
-  // expect(event.args.tokenId.toNumber()).to.be.above(0)
-  // it('Should have minted Link tokens', async function () {
-  //   expect(await linkContract.balanceOf(owner.address)).to.equal(
-  //     '1000000000000000000000000'
-  //   )
-  // })
-  // describe('addCurrency', function () {
-  //   it('Should add a token to the token list if owner', async function () {
-  //     await gnft.addCurrency(
-  //       linkContract.address,
-  //       ethers.utils.parseEther('1'),
-  //       'Link 3'
-  //     )
-  //     const approvedTokens = await gnft.getApprovedTokenInfo()
-
-  //     expect(approvedTokens[0][0]).to.equal(linkContract.address)
-  //     expect(approvedTokens[1][0]).to.equal('1000000000000000000')
-  //     expect(approvedTokens[2][0]).to.equal('Link 3')
-  //     expect(await gnft.currenciesAdded()).to.equal(1)
-  //   })
-  //   it('Should NOT add a token to the token list if NOT owner', async function () {
-  //     await expect(
-  //       gnft
-  //         .connect(addr1)
-  //         .addCurrency(
-  //           linkContract.address,
-  //           ethers.utils.parseEther('1'),
-  //           'Link 3'
-  //         )
-  //     ).to.be.revertedWith('Ownable: caller is not the owner')
-  //   })
-  // })
-  // describe('mint', function () {
-  //   it('Should mint the NFT', async function () {
-  //     await gnft.addCurrency(
-  //       linkContract.address,
-  //       ethers.utils.parseEther('1'),
-  //       'Link 3'
-  //     )
-
-  //     await linkContract.approve(
-  //       gnft.address,
-  //       ethers.utils.parseEther('100')
-  //     )
-
-  //     await gnft.mint(addr1.address, 1, 0)
-
-  //     expect(await gnft.balanceOf(addr1.address)).to.equal(1)
-  //   })
-  //   it('Should mint with not owner addr ', async function () {
-  //     await gnft.addCurrency(
-  //       linkContract.address,
-  //       ethers.utils.parseEther('1'),
-  //       'Link 3'
-  //     )
-
-  //     await linkContract
-  //       .connect(addr1)
-  //       .approve(gnft.address, ethers.utils.parseEther('100'))
-
-  //     await linkContract.approve(owner.address, ethers.utils.parseEther('100'))
-  //     await linkContract.transferFrom(
-  //       owner.address,
-  //       addr1.address,
-  //       ethers.utils.parseEther('100')
-  //     )
-
-  //     await gnft.connect(addr1).mint(addr1.address, 1, 0)
-  //     expect(await gnft.balanceOf(addr1.address)).to.equal(1)
-  //   })
-  //   it('Should NOT mint if not enough ERC20 funds ', async function () {
-  //     await gnft.addCurrency(
-  //       linkContract.address,
-  //       ethers.utils.parseEther('1000'),
-  //       'Link 3'
-  //     )
-  //     await linkContract.approve(owner.address, ethers.utils.parseEther('100'))
-  //     await linkContract.transferFrom(
-  //       owner.address,
-  //       addr1.address,
-  //       ethers.utils.parseEther('100')
-  //     )
-
-  //     await linkContract
-  //       .connect(addr1)
-  //       .approve(gnft.address, ethers.utils.parseEther('100'))
-
-  //     await expect(
-  //       gnft.connect(addr1).mint(addr1.address, 1, 0)
-  //     ).to.be.revertedWith('ERC20: insufficient allowance')
-  //   })
-  // })
+  describe('Withdraw', function () {
+    let baseUri, fileNames, totalFee
+    beforeEach(async () => {
+      baseUri = 'QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG'
+      fileNames = ['file1.jpg', 'file2.jpg']
+      totalFee = utils.parseEther('10')
+      await gnft.setFee(totalFee)
+      await gnft.connect(addr1).mint(addr1.address, baseUri, fileNames, {
+        value: utils.parseEther('20'),
+      })
+    })
+    it('should have funds from minting', async () => {
+      expect(await ethers.provider.getBalance(gnft.address)).to.equal(
+        utils.parseEther('20')
+      )
+    })
+    it('should transfer funds to owner', async () => {
+      await gnft.withdraw()
+      expect(await ethers.provider.getBalance(gnft.address)).to.equal(
+        utils.parseEther('0')
+      )
+    })
+    it('should NOT transfer funds to other addr', async () => {
+      await expect(gnft.connect(addr1).withdraw()).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+    })
+  })
 })
