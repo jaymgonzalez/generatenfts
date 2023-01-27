d8188f698d27e9bcf9bed789b072488668759961
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-27 12:26:50 +0100

    add matic faucet

--- front-end/components/WorkInProgressOverlay.tsx
+++ front-end/components/WorkInProgressOverlay.tsx
@@ -12,6 +12,8 @@ import {
 } from '@mantine/core'
 import { IconAlertCircle } from '@tabler/icons'
 import { Metamask } from '@web3uikit/icons'
+import Link from 'next/link'
+import { Matic } from '@web3uikit/icons'
 
 const useStyles = createStyles(() => ({
   modal: {
@@ -62,14 +64,26 @@ export default function WIPOverlay() {
             color="yellow"
           >
             <Text align="center">
-              There are parts of the site still under construction 🚧👷🏽‍♀️👷🏽‍♂️🚧
+              There are parts of the site still under construction
             </Text>
-            <Text align="center">Please proceed with caution.</Text>
-            <Text align="center">
-              At the moment, only Polygon Mumbai is working.
+            <Text align="center">🚧👷🏽‍♀️👷🏽‍♂️🚧</Text>
+            <Text
+              align="center"
+              sx={{
+                a: {
+                  textDecoration: 'none',
+                },
+              }}
+            >
+              Please check our guide on how the app works{' '}
+              <Link href="/support">here</Link>
             </Text>
             <Text align="center">
-              Use the button bellow to add it to your Metamask.
+              At the moment, only live on Polygon Mumbai.
+            </Text>
+            <Text align="center">
+              Use the buttons bellow to add the network to your Metamask and
+              claim some TEST MATIC.
             </Text>
           </Alert>
           <Group position="center" py={16}>
@@ -79,6 +93,17 @@ export default function WIPOverlay() {
               onClick={() => addMaticNetwork()}
             >
               Add Polygon Mumbai network to Metamask
+            </Button>
+          </Group>
+          <Group position="center" pb={16}>
+            <Button
+              component="a"
+              variant="outline"
+              leftIcon={<Matic fontSize={20} />}
+              target="_blank"
+              href="https://mumbaifaucet.com/"
+            >
+              Add Test Matic to your wallet
             </Button>
           </Group>
         </Modal>
